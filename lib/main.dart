726a1df80a741a9f09201aae2f9781805303dc42
Sayantan Sadhu <dip.sadhu111@gmail.com>
2024-04-08 22:53:58 +0530

    work

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,40 @@
+import 'package:blogapp/Pages/HomePage.dart';
 import 'package:blogapp/Pages/WelcomePage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget page = WelcomePage();
+  final storage = FlutterSecureStorage();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkLogin();
+  }
+
+  void checkLogin() async {
+    String? token = await storage.read(key: "token");
+    if (token != null) {
+      setState(() {
+        page = HomePage();
+      });
+    } else {
+      setState(() {
+        page = WelcomePage();
+      });
+    }
+  }
 
   // This widget is the root of your application.
   @override
