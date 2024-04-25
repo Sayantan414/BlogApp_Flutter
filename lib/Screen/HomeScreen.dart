f414614435fa15e21eb302a4250924cb337e95ca
Sayantan Sadhu <dip.sadhu111@gmail.com>
2024-04-25 16:21:20 +0530

    work

--- lib/Screen/HomeScreen.dart
+++ lib/Screen/HomeScreen.dart
@@ -9,57 +9,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool _isSearching = false;
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: Color.fromARGB(98, 197, 222, 184),
-      // appBar: AppBar(
-      //   backgroundColor: Color.fromARGB(97, 206, 224, 197),
-      //   automaticallyImplyLeading: false, // Removes the back button
-      //   actions: [
-      //     _isSearching
-      //         ? Expanded(
-      //             child: Row(
-      //               children: [
-      //                 Expanded(
-      //                   child: Padding(
-      //                     padding: const EdgeInsets.only(left: 10.0),
-      //                     child: TextField(
-      //                       decoration: InputDecoration(
-      //                         hintText: 'Search...',
-      //                         border: InputBorder.none,
-      //                       ),
-      //                       autofocus: true,
-      //                       textInputAction: TextInputAction.search,
-      //                       onSubmitted: (value) {
-      //                         // Perform search here with value
-      //                       },
-      //                     ),
-      //                   ),
-      //                 ),
-      //                 IconButton(
-      //                   icon: Icon(Icons.cancel),
-      //                   onPressed: () {
-      //                     setState(() {
-      //                       _isSearching = false;
-      //                     });
-      //                   },
-      //                 ),
-      //               ],
-      //             ),
-      //           )
-      //         : IconButton(
-      //             icon: Icon(Icons.search),
-      //             onPressed: () {
-      //               setState(() {
-      //                 _isSearching = true;
-      //               });
-      //             },
-      //           ),
-      //   ],
-      // ),
       body: SingleChildScrollView(
         child: Blogs(url: "/blogpost/getOtherBlog", type: "Public"),
       ),
